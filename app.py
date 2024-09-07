b486b74eed3e9d2f9cf31565e2cf7581d71fdfb1
Shivam-Panda <panda.shivam81@bloomfield.org>
2024-09-07 08:17:56 -0400

    Numbers

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ def michigan():
                 if len(data) < 1000:
                     data.append({
                         "rate": row[2],
-                        "coordinates": [lat, long[:len(long)-1]]
+                        "coordinates": [float(lat), float(long[:len(long)-1])]
                     })
             except:
                 print("ERROR")
