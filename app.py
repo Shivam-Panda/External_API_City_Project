057452af92c6f54fa78a677b24837ee991eaa523
Shivam-Panda <panda.shivam81@bloomfield.org>
2024-09-07 01:01:20 -0400

    Michigan Data Done

--- app.py
+++ app.py
@@ -1,10 +1,31 @@
 from flask import Flask
+import csv
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return 'Hello'
+
+@app.route('/mi')
+def michigan():
+    data = []
+    with open('./mi.csv') as file:
+        reader = csv.reader(file)
+        for row in reader:
+            split = row[3].split('(')
+            try:
+                [lat, long] = split[1].split(' ')
+                data.append({
+                    "rate": row[2],
+                    "point": {
+                        "latitude": lat,
+                        "longitude": long[:len(long)-1]
+                    }
+                })
+            except:
+                print("ERROR")
+    return data
 
 if __name__ == '__main__':
     app.run(debug=True)
